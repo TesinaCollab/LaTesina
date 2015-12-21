74f6bf66132824be12ad1fb78ba2982fec0d7f77
Daniele <iximiel@gmail.com>
2015-12-21 22:21:42 +0100

    First commit

---
+++ states.v
@@ -0,0 +1,12 @@
+module states(
+	input clk;
+	input reset;
+);
+reg stateholder;
+always@(posedge clk or negedge reset) begin
+	if(!reset)
+		stateholder <= 0;
+	else
+		stateholder <= 1;
+	end
+endmodule
