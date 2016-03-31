663b000aabd27c1d18910fc1cd4d7b03e3dd6a4a
AlexAmato <alexamato@outlook.it>
2016-03-31 18:50:28 +0200

    corretti orrori

--- quadrato.v
+++ quadrato.v
@@ -19,7 +19,7 @@ wire xDiff = larg2- X_POS;
 assign xUnder = (X_POS < larg2);
 assign yUnder = Y_POS < alt2;
 wire orizz,vert;
-assign orizz = (xUnder)?((X_CONTROLLO > 0) && (X_CONTROLLO +< (X_POS + larg2))||(X_CONTROLLO > (H-diff))):((X_CONTROLLO > X_POS-larg2) && (X_CONTROLLO < (X_POS + larg2)));
+assign orizz = (xUnder)?((X_CONTROLLO > 0) && (X_CONTROLLO < (X_POS + larg2))||(X_CONTROLLO > (H-xDiff))):((X_CONTROLLO > X_POS-larg2) && (X_CONTROLLO < (X_POS + larg2)));
 assign vert = ((Y_CONTROLLO + alt2 > Y_POS) && (Y_CONTROLLO < (Y_POS + alt2)));
 assign CONFERMA = orizz && vert;
 		
