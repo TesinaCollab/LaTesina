59d3c89564daa7a0f87b48f25c9a0b91415e8788
Iximiel <iximiel@gmail.com>
2016-04-02 17:30:54 +0200

    in ordine anche le figure

--- quadrato.v
+++ quadrato.v
@@ -1,77 +1,104 @@
+/*Disegna un rettangolo
+ * Se i parametri di controllo sono all'interno del rettangolo
+ * l'ouput CONTROLLO e` alto
+ * in piu` se in rettangolo si sovrappone a un lato dello schermo viene 
+ * visualizato su quello opposto
+ */
+
 module rettangolo(
-//Posizione del centro
-input [10:0] X_POS,
-input [10:0] Y_POS,
-//Controllo
-input [10:0] X_CONTROLLO,
-input [10:0] Y_CONTROLLO,
+		  //Posizione del centro
+		  input [10:0] X_POS,
+		  input [10:0] Y_POS,
+		  //Controllo
+		  input [10:0] X_CONTROLLO,
+		  input [10:0] Y_CONTROLLO,
 
-output CONFERMA
-);
+		  output       CONFERMA
+		  );
+   //=============================
+   //parametri
+   //=============================
+   
+   parameter altezza = 100;
+   parameter larghezza = 100;
+   parameter H = 1280;
+   parameter V = 1024;
 
-parameter altezza = 100;
-parameter larghezza = 100;
-parameter H = 1280;
-parameter V = 1024;
+   //=============================
+   // Fili
+   //=============================
+
+   wire [10:0] 		       xDiff = H - X_POS;
+   wire [10:0] 		       yDiff = V - Y_POS;
+   wire 		       xUnder, yUnder;
+
+   wire 		       orizz,vert;
+
+   //=============================
+   //Struttura
+   //=============================
+
+   assign xUnder = (xDiff < larghezza);
+   assign yUnder = (yDiff < altezza);
 
 
-wire [10:0]xDiff = H - X_POS;
-wire [10:0]yDiff = V - Y_POS;
-wire xUnder, yUnder;
+   assign orizz = (xUnder)?
+		  ((X_CONTROLLO > X_POS)||(X_CONTROLLO < (larghezza - xDiff)))
+     :((X_CONTROLLO > X_POS) && (X_CONTROLLO < (X_POS + larghezza)));
 
-wire orizz,vert;
-	
+   assign vert = (yUnder)?
+		 ((Y_CONTROLLO > Y_POS)||(Y_CONTROLLO < (altezza - yDiff)))
+     :((Y_CONTROLLO > Y_POS) && (Y_CONTROLLO < (Y_POS + altezza)));
 
-assign xUnder = (xDiff < larghezza);
-assign yUnder = (yDiff < altezza);
-
-
-assign orizz = (xUnder)?
-((X_CONTROLLO > X_POS)||(X_CONTROLLO < (larghezza - xDiff)))
-:((X_CONTROLLO > X_POS) && (X_CONTROLLO < (X_POS + larghezza)));
-
-assign vert = (yUnder)?
-((Y_CONTROLLO > Y_POS)||(Y_CONTROLLO < (altezza - yDiff)))
-:((Y_CONTROLLO > Y_POS) && (Y_CONTROLLO < (Y_POS + altezza)));
-
-assign CONFERMA = orizz && vert;
-		
+   assign CONFERMA = orizz && vert;
+   
 
 endmodule //rettangolo
 
 module cornicetta(
-//Posizione del centro
-input [10:0] X_POS,
-input [10:0] Y_POS,
-//Controllo
-input [10:0] X_CONTROLLO,
-input [10:0] Y_CONTROLLO,
+		  //Posizione del centro
+		  input [10:0] X_POS,
+		  input [10:0] Y_POS,
+		  //Controllo
+		  input [10:0] X_CONTROLLO,
+		  input [10:0] Y_CONTROLLO,
 
-output CONFERMA, //disegna solo la cornicetta
-output esterno, //conferma del rettangolo esterno 
-output interno //conferma del rettangolo interno 
-);
+		  output       CONFERMA, //disegna solo la cornicetta
+		  output       esterno, //conferma del rettangolo esterno 
+		  output       interno //conferma del rettangolo interno 
+		  );
+   //=============================
+   //parametri
+   //=============================
+   
+   parameter altezza = 100;
+   parameter larghezza = 100;
+   parameter spessore = 6;
+   parameter H = 1280;
+   parameter V = 1024;
+   //non modificare:
+   parameter altint = altezza - spessore;
+   parameter largint = larghezza - spessore;
+   
+   //=============================
+   //Fili
+   //=============================
+   
+   wire 		       out, in;
+   parameter spessore2 = spessore / 2;
+   wire [10:0] 		       Xint = ((X_POS + spessore2)>H)?(X_POS + spessore2-H):(X_POS + spessore2);
+   wire [10:0] 		       Yint = ((Y_POS + spessore2)>V)?(Y_POS + spessore2-V):(Y_POS + spessore2);
+   
+   //=============================
+   //Struttura
+   //=============================
+   assign esterno = out;
+   assign interno = in;
 
-wire out, in;
-assign esterno = out;
-assign interno = in;
 
-parameter altezza = 100;
-parameter larghezza = 100;
-parameter spessore = 6;
-parameter H = 1280;
-parameter V = 1024;
+   rettangolo#(altezza,larghezza,H,V) attorno(X_POS,Y_POS,X_CONTROLLO,Y_CONTROLLO,out);
+   rettangolo#(altint,largint,H,V) dentro(Xint,Yint,X_CONTROLLO,Y_CONTROLLO,in);
 
-parameter spessore2 = spessore / 2;
-wire [10:0] Xint = ((X_POS + spessore2)>H)?(X_POS + spessore2-H):(X_POS + spessore2);
-wire [10:0] Yint = ((Y_POS + spessore2)>V)?(Y_POS + spessore2-V):(Y_POS + spessore2);
-parameter altint = altezza - spessore;
-parameter largint = larghezza - spessore;
-
-
-rettangolo#(altezza,larghezza,H,V) attorno(X_POS,Y_POS,X_CONTROLLO,Y_CONTROLLO,out);
-rettangolo#(altint,largint,H,V) dentro(Xint,Yint,X_CONTROLLO,Y_CONTROLLO,in);
-
-assign CONFERMA = (out)? out && !in :0 ;
+   assign CONFERMA = (out)? out && !in :0 ;
 
 endmodule
--- tringolo.v
+++ tringolo.v
@@ -1,19 +1,44 @@
+/**Disegna un triangolo rettangolo isoscele*****
+ * Il modulo riceve in input la posizione dell'angolo superiore del triangolo
+ * e il punto attuale dello schermo
+ * l'uscita e` positiva se la posizione del controllo e` all'interno del
+ * triangolo
+ * |\
+ * | \
+ * ----
+ ***********************************************/
 module triangolo(
-//Posizione del centro
-input [10:0] X_POS,
-input [10:0] Y_POS,
-//Controllo
-input [10:0] X_CONTROLLO,
-input [10:0] Y_CONTROLLO,
+		 //Posizione del centro
+		 input [10:0] X_POS,
+		 input [10:0] Y_POS,
+		 //Controllo
+		 input [10:0] X_CONTROLLO,
+		 input [10:0] Y_CONTROLLO,
 
-output CONFERMA
-);
-parameter lato = 100;
-wire okY, okX;
-wire [10:0]diff;
-assign diff = Y_CONTROLLO-Y_POS;
-assign okY = (Y_CONTROLLO>Y_POS)&&(Y_CONTROLLO<(Y_POS+lato));
-assign okX = (X_CONTROLLO>X_POS)&&(X_CONTROLLO<(X_POS+diff));
-assign CONFERMA = okY && okX;
+		 output       CONFERMA
+		 );
+   //=============================
+   //parametri
+   //=============================
+   
+   parameter lato = 100;
+   
+   //=============================
+   //fili
+   //=============================
+   
+   wire 		      okY, okX;
+   wire [10:0] 		      diff;
+   
+   //=============================
+   //struttura
+   //=============================
+   
+   assign diff = Y_CONTROLLO-Y_POS;
+   
+   assign okY = (Y_CONTROLLO>Y_POS)&&(Y_CONTROLLO<(Y_POS+lato));
+   assign okX = (X_CONTROLLO>X_POS)&&(X_CONTROLLO<(X_POS+diff));
+   
+   assign CONFERMA = okY && okX;
 
 endmodule
